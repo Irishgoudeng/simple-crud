c972d9cc89cc8828479cfa0acee742ae41b90a6a
irishgoudeng <vincewarpie@gmail.com>
2024-09-15 12:48:30 +0800

    Dev commit test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+
+const path = require("path");
+const sqlitePath = path.resolve(__dirname, "prisma/dev.db");
+
+module.exports = {
+  env: {
+    DATABASE_URL: sqlitePath,
+  },
+};
 
 export default nextConfig;
